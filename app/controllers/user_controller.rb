e85471df1abd2e1a8a33def5e15f3f21de243a40
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-03-11 00:12:19 +0530

    FEAT:  Added login feature

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,12 +5,45 @@ class UserController < ApplicationController
     render plain: User.all.map { |user| user.to_string }.join("\n")
   end
 
+  def hash_password!(password)
+    # Add some custom logic or use some library like bcrypt
+    "#{Time.now.getutc}^#{password}"
+  end
+
+  def compare_password(db_password, entered_password)
+    pass = db_password.split("^")[1]
+    if pass == entered_password
+      true
+    else
+      false
+    end
+  end
+
   def create
-    # Hash the password before adding!
-    new_user = User.create!(name: params[:name], email: params[:email], password: params[:password])
-    render plain: "A new User with id: #{new_user.id} has been created!"
+    # Check for empty values
+    name = params[:name].empty?
+    email = params[:email].empty?
+    password = params[:password].empty?
+    puts "#{name}, #{email}, #{password}"
+    if !name && !email && !password
+      # Hash the password before adding!
+      encrypted_password = hash_password!(params[:password])
+      new_user = User.create!(name: params[:name], email: params[:email], password: encrypted_password)
+      render plain: "A new User with id: #{new_user.id} has been created!"
+    else
+      render plain: "Please enter all the required fields"
+    end
   end
 
   def login
+    email = params[:email]
+    pass = params[:password]
+    user = User.where("email = ?", email).first
+
+    if user
+      render plain: compare_password(user.password, pass)
+    else
+      render plain: "No user found"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "user/login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "todos", to: "todos#index"
-  # get "todos/:id", to: "todos#show"
+  post "user/login", to: "user#login"
+
   resources :todos
   resources :user
 end
