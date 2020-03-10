a207310b4eb980f52cafe7980eb8cb553770847d
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-03-10 23:26:26 +0530

    FEAT: Added List and Create User

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,14 @@
 class UserController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+    render plain: User.all.map { |user| user.to_string }.join("\n")
   end
 
   def create
+    # Hash the password before adding!
+    new_user = User.create!(name: params[:name], email: params[:email], password: params[:password])
+    render plain: "A new User with id: #{new_user.id} has been created!"
   end
 
   def login
