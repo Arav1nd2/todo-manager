aeca3f48898cdbbf95416dbf4867190723f7848b
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-03-10 22:27:34 +0530

    FEAT: Added update and patch routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.map { |todo| todo.to_pleasent_string }.join("\n")
   end
@@ -6,5 +8,21 @@ class TodosController < ApplicationController
   def show
     todo = Todo.find(params[:id]).to_pleasent_string
     render plain: "#{todo}"
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    render plain: "A new Todo has been created with id: #{new_todo.id}"
+  end
+
+  def update
+    new_status = params[:status]
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.completed = new_status
+    todo.save
+    render plain: "Todo with id : #{id} update as : Completed: #{new_status}"
   end
 end
