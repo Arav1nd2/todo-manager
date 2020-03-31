492f659842be0857018f8a6982ed2ec75da8e754
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-03-31 20:31:51 +0530

    FEAT: Todos View Completed

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,20 @@ class Todo < ActiveRecord::Base
     finished = completed == false ? "[ ]" : "[X]"
     "#{id}. #{finished} #{todo_text} , #{due_date.to_s(:short)}"
   end
+
+  def self.overdue
+    all.where("due_date < ? and completed = ?", Date.today, false)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where("completed = ?", true)
+  end
 end
